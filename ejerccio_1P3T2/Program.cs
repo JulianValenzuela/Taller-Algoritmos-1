a60a28cbbc2866f5fe4871b50e75561e11e27022
Julián Valenzuela <julianandresvalenzuela21@gmail.com>
2022-02-23 17:38:35 -0500

    Agregar archivos de proyecto.

---
+++ ejerccio_1P3T2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ejerccio_1P3T2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un letra: ");
+            string letra = Console.ReadLine();
+
+            string rest = Vocales(letra);
+
+            if (rest.Equals("vocal")) Console.WriteLine($"Es vocal");
+
+            else if (rest.Equals("consonante")) Console.WriteLine($"Es consonante");
+        }
+        static string Vocales(string letrita)
+        {
+            string letra = letrita.ToLower();
+            string rest = "";
+
+            if (letra.Equals("a") || letra.Equals("e") || letra.Equals("i") || letra.Equals("o") || letra.Equals("u"))
+
+                rest = "vocal";
+
+            else rest = "consonante";
+
+                return rest;
+
+        }
+    }
+}
